5e381320f14f1d483c2aa5a3db12eec8b8664e64
Jackiele123 <43102053+Jackiele123@users.noreply.github.com>
2022-11-20 16:03:08 -0500

    Trained image dataset

--- data_set.py
+++ data_set.py
@@ -5,12 +5,15 @@ import PIL.Image
 import tensorflow as tf
 import pathlib
 import matplotlib.pyplot as plt
+import glob
 
 batch_size = 32
-img_height = 180
-img_width = 180
+img_height = 200
+img_width = 200
 
-data_dir = "data"
+data_dir = pathlib.Path("test_set/alphabet_train")
+
+image_count = len(list(data_dir.glob('*/*.jpg')))
 
 train_ds = tf.keras.utils.image_dataset_from_directory(
   data_dir,
@@ -28,12 +31,63 @@ val_ds = tf.keras.utils.image_dataset_from_directory(
   image_size=(img_height, img_width),
   batch_size=batch_size)
 
+
+class_names = train_ds.class_names
+print(class_names)
+
+#rescale rgb value range [0,255] to [0,1]
+normalization_layer = tf.keras.layers.Rescaling(1./255)
+normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+image_batch, labels_batch = next(iter(normalized_ds))
+
 AUTOTUNE = tf.data.AUTOTUNE
 
-train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
-val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+num_classes = 29
 
-num_classes = 26
+list_ds = tf.data.Dataset.list_files(str(data_dir/'*/*'), shuffle=False)
+list_ds = list_ds.shuffle(image_count, reshuffle_each_iteration=False)
+
+class_names = np.array(sorted([item.name for item in data_dir.glob('*')]))
+print(class_names)
+
+val_size = int(image_count * 0.2)
+train_ds = list_ds.skip(val_size)
+val_ds = list_ds.take(val_size)
+
+def get_label(file_path):
+  # Convert the path to a list of path components
+  parts = tf.strings.split(file_path, os.path.sep)
+  # The second to last is the class-directory
+  one_hot = parts[-2] == class_names
+  # Integer encode the label
+  return tf.argmax(one_hot)
+
+def decode_img(img):
+  # Convert the compressed string to a 3D uint8 tensor
+  img = tf.io.decode_jpeg(img, channels=3)
+  # Resize the image to the desired size
+  return tf.image.resize(img, [img_height, img_width])
+
+def process_path(file_path):
+  label = get_label(file_path)
+  # Load the raw data from the file as a string
+  img = tf.io.read_file(file_path)
+  img = decode_img(img)
+  return img, label
+
+# Set `num_parallel_calls` so multiple images are loaded/processed in parallel.
+train_ds = train_ds.map(process_path, num_parallel_calls=AUTOTUNE)
+val_ds = val_ds.map(process_path, num_parallel_calls=AUTOTUNE)
+
+def configure_for_performance(ds):
+  ds = ds.cache()
+  ds = ds.shuffle(buffer_size=1000)
+  ds = ds.batch(batch_size)
+  ds = ds.prefetch(buffer_size=AUTOTUNE)
+  return ds
+
+train_ds = configure_for_performance(train_ds)
+val_ds = configure_for_performance(val_ds)
 
 model = tf.keras.Sequential([
   tf.keras.layers.Rescaling(1./255),
@@ -53,8 +107,12 @@ model.compile(
   loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
   metrics=['accuracy'])
 
-model.fit(
-  train_ds,
-  validation_data=val_ds,
-  epochs=3
-)  
+checkpoint_path = "training_1/cp.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+# Create a callback that saves the model's weights
+cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                 save_weights_only=True,
+                                                 verbose=1)
+
+model.fit(train_ds, validation_data=val_ds, epochs=20, callbacks=[cp_callback])                                                   
